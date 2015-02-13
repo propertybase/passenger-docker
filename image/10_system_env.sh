9fd1d1d7a5886f2e19b3361b597e4a7c95b4ba40
Konstantin Krauss <konstantin@propertybase.com>
2015-02-13 14:05:23 +0100

    Remove logger workflow entirly

--- image/10_system_env.sh
+++ image/10_system_env.sh
@@ -4,7 +4,7 @@
 
 ## Sidekiq
 
-mkdir -p /etc/service/sidekiq/log
+mkdir -p /etc/service/sidekiq
 touch /etc/service/sidekiq/down
 cat > /etc/service/sidekiq/run <<EOF
 #!/bin/sh
@@ -12,15 +12,11 @@ exec 2>&1
 cd /home/app/code
 exec chpst -u app bundle exec sidekiq -e $PASSENGER_APP_ENV
 EOF
-cat > /etc/service/sidekiq/log/run <<EOF
-#!/bin/sh
-exec logger -t sidekiq
-EOF
-chmod +x /etc/service/sidekiq/run /etc/service/sidekiq/log/run
+chmod +x /etc/service/sidekiq/run
 
 ## Clockwork
 
-mkdir -p /etc/service/clockwork/log
+mkdir -p /etc/service/clockwork
 touch /etc/service/clockwork/down
 cat > /etc/service/clockwork/run <<EOF
 #!/bin/sh
@@ -28,8 +24,4 @@ exec 2>&1
 cd /home/app/code
 exec chpst -u app bundle exec clockwork $CLOCKWORK_FILE
 EOF
-cat > /etc/service/clockwork/log/run <<EOF
-#!/bin/sh
-exec logger -t clockwork
-EOF
-chmod +x /etc/service/clockwork/run /etc/service/clockwork/log/run
+chmod +x /etc/service/clockwork/run
